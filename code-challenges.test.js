636f67a1dc49a9fac033e9f3bd963beb1b872330
kevans107 <kellyevans107.gmail.com>
2021-10-11 10:16:56 -0400

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -23,9 +24,30 @@ var secretCodeWord2 = "Gobbledygook"
 var secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("codedMessage", () => {
+    it("that takes in a string and returns a coded message", () => {
+        expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
 
+// ReferenceError: codedMessage is not defined
 
 // b) Create the function that makes the test pass.
+
+const codedMessage = (string) => {
+    // Create a function called codedMessage  that takes in a string as a parameter 
+    finalString = ''
+    // Create an empty string to contain the new string since .replace does not alter the original string
+    for(let i=0; i < string.length; i++){
+        // Iterate through the string with a for loop
+        finalString = string.replace(/([aA])/g, "4").replace(/([eE])/g, '3').replace(/([iI])/g, '1').replace(/[oO]/g, '0')
+        }
+        // Use the .replace method with regex to replace each vowel with the corresponding number. Perform a global replacement to replace all instances of the vowel.
+        return finalString
+    }
+
 
 
 
@@ -41,8 +63,27 @@ var letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 
+describe("particularLetter", () => {
+    it("takes in an array of words and a single letter and returns all the words that contain that particular letter", () => {
+        expect(particularLetter(arrayOfWords1, letterA)).toEqual(["Apple", "Banana", "Orange"])
+        expect(particularLetter(arrayOfWords2, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
 // b) Create the function that makes the test pass.
+var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+var letterA = "a"
+
+const particularLetter = (array, letter) => {
+    // Create a function called particularLetter with an array and a single letter as parameters
+    return array.filter(function(filterLetter) { 
+        // Use .filter higher order function to filter through the array
+        return filterLetter.toLowerCase().indexOf(letter.toLowerCase()) !== -1
+        // Create a new function called filterLetter that compares the lowercase array letter at each index with the input letter parameter. If the value is found the function will return the string in the output
+    }) 
+}
+    
+
 
 
 
@@ -57,6 +98,34 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
-
+describe("fullHouse", () => {
+    it("function that takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        expect(fullHouse([5, 5, 5, 3, 3])).toEqual(true)
+        expect(fullHouse([5, 5, 3, 3, 4])).toEqual(false)
+        expect(fullHouse([5, 5, 5, 5, 4])).toEqual(false)
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+
+const fullHouse = (array) => {
+    // Create a function called fullHouse that takes in an array as a parameter
+    let houseArray=[]
+    // Create an empty array to push the iterated return to
+    const counts = {};
+    // Create an object called counts to show what the count is for each index
+    for (const num of array) {
+      counts[num] = counts[num] ? counts[num] + 1 : 1
+    } // Create a loop with for...of to iterate over the array to compare counts with a ternary operator
+    for(const key in counts){
+        // Us for...in to count the number of instances for each value in the object
+        houseArray.push(counts[key])
+        // push the key and the counts for each value to the empty houseArray
+    }
+    for(let i=0;i<houseArray.length;i++){
+      if(houseArray[i]===3)return true
+      // Use for loop to iterate through the houseArray, if one value is only returned three times, return true
+    }return false
+    // else return false
+}
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,17 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def single_letter(array, value)
+    # Create a method called single_letter
+        array.select { |string| string.include?(value)}
+    # Use the .select to iterate through the array
+    # Inside the block use the .include? to return a boolean of whether the string includes a specific character
+    end 
+
+p single_letter beverages_array,letter_o
+p single_letter beverages_array, letter_t
+
+
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
 
@@ -19,16 +30,65 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+def return_sum array
+    # create a method called return_sum that has an array as a parameter
+    array.inject { |sum, num| sum + num}
+    # use the enumerator method .inject to take the zero index of the array and the first index and adds them together, continuing down the array until all numbers have passed through the block
+    # returns the sum of all the numbers in the array 
+end 
 
+return_sum nums_array1
+return_sum nums_array2
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(model, wheels, current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+    def bike_info
+        p "The Trek bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+end
 
+my_bike = Bike.new("schwinn",2,10)
+my_bike.bike_info
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(10) => 20
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+    # attr_accessor :model, :wheels, :current_speed, 
+    def initialize(model, wheels, current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+        @pedal_faster = 10
+        @brake = 0
+    end
+    def bike_info
+        p "The Trek bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+    def pedal_faster(pedal_faster)
+        p @current_speed = @current_speed + @pedal_faster
+    end
+    def pedal_brake(brake)
+        if @current_speed >= 0
+            p @current_speed = @current_speed - @brake
+        end
+    end
+end
+
+my_bike = Bike.new("schwinn",2,10)
+my_bike.bike_info
+my_bike.pedal_faster(10)
+my_bike.pedal_faster(10)
+my_bike.pedal_brake(15)
